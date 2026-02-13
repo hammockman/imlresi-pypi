45da7f46c6070ea74ad1690d78a04b1be230efc6
jonoh <jh@aliente.ch>
2026-02-13 15:39:25 +1300

    bump package version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='imlresi',
-    version='0.0.7',
+    version='0.0.8',
     license='MIT',
     description='Tools for dealing with IML-Resi PowerDrill data.',
     long_description=read('README.md'),
